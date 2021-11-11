c838a2b3a5dd3c6fdd808cf0905203503ebeea96
bhola <4bholagupta@gmail.com>
2021-11-11 21:40:51 +0530

    declearing/linking sperate views file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,26 @@
 class ProductsController < ApplicationController
 
-  before_action  :set_product, only: %i[show edit update destroy]
-  before_action  :user_is_admin, only: %i[create edit update destroy]
 
   def index
     @products = Product.all
     # redirect_to root_url
   end
   
-    def show
-      @product = Product.find(params[:id])
-    end
- 
-    def new
-      @product = Product.new
-      
-    end
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
 
   def create
     @product = Product.new(product_params)
-    # respond_to do |format|
-      if @product.save
-        # product_save_success_response(format,
-        #                               'Product was successfully created.')
-        redirect_to @product
-      else
-        # product_save_failure_response(format, :new)
-        render :new
-      end
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
 
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,8 +2,3 @@ class LineItem < ApplicationRecord
 end
 
 
-# it take the item in sequence
-# bin/rails generate model line_items quantity:integer product_id:integer   cart_id:integer  order_id:integer 
-
-
-$ bin/rails generate controller static_pages index --skip-routes    
