63a03c1c2c2bef2b03153e862588a92cd71e2084
bhola <4bholagupta@gmail.com>
2021-11-12 15:54:57 +0530

    adding the button and solve some error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :products, dependent: :destroy
-  has_many :products, dependent: :destroy
-
+  validates :name, presence: true 
+  validates :details, presence: true, length: { minimum: 10  } 
 
 end
